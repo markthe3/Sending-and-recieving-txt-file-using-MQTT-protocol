a9d2517827f60c9d565e1af1a9b1f9b5a5fbb4ea
hamzathe3 <124628723+markthe3@users.noreply.github.com>
2023-08-10 14:34:08 +0100

    Update and rename mqtt_publish1.py to mqtt_publisher.py

---
+++ mqtt_publisher.py
@@ -0,0 +1,15 @@
+import paho.mqtt.client as mqtt
+import time
+
+mqttBroker = "mqtt.eclipseprojects.io"
+client = mqtt.Client("TextFile_Publisher")
+client.connect(mqttBroker)
+
+file_path = "C:/Users/PC/OneDrive/Bureau/test.txt"  # Specify the path to your text file
+
+with open(file_path, "r") as file:
+    text_content = file.read()
+    client.publish("TEXT_FILE", text_content)
+    print("Text file content published to Topic TEXT_FILE")
+
+client.disconnect()
